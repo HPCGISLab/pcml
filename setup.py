89ac850de9c5a7cc01ba446fc57e57ef95f01ea8
Kevin Whalen <kwhalen2@kent.edu>
2014-10-02 18:03:02 -0400

    package setup, metadata update; non-essential

--- setup.py
+++ setup.py
@@ -13,10 +13,35 @@ install_reqs = parse_requirements("./requirements.txt")
 reqs = [str(ir.req) for ir in install_reqs]
 
 if __name__ == '__main__':
+    # https://docs.python.org/2/distutils/setupscript.html#additional-meta-data
     setup(
           name='pcml',
           version='0.1',
+          description='The parallel cartographic modeling language (pCML) provides spatial operations while hiding the implementation complexities of parallelism.',
+          url='https://github.com/HPCGISLab/pcml',
+          author='Eric Shook',
+          author_email='eshook@kent.edu',
           license='Copyright (c) 2014 High-Performance Computing and GIS (HPCGIS) Laboratory. All rights reserved. Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.',
+          long_description=
+"""
+pCML
+====
+
+The parallel cartographic modeling language (pCML) is a multi-institutional 
+collaborative project aiming to create a computing language for 
+cyberGIScientists that is designed for (1) usability, (2) programmability, and 
+(3) scalability. pCML provides multi-core parallel processing for spatial 
+operations while hiding the implementation complexities of parallelism.
+
+**Author**
+
+Eric Shook <eshook@kent.edu>
+
+**Contributors**
+
+* Zhengliang Feng (odayfans@gmail.com, zfeng2@kent.edu)
+""",
+          #platform=[''],
           install_requires = reqs,
           packages=['pcml', 'pcml.core', 'pcml.lib', 'pcml.util'],
           test_suite = 'tests',
