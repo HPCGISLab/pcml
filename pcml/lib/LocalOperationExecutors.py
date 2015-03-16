c2fc6b41909ba0b3f03e58c7f6d83ea7ade61436
Sandeep Vutla <svutla@kent.edu>
2015-03-16 09:08:45 -0400

    updated some local operations

--- pcml/lib/LocalOperationExecutors.py
+++ pcml/lib/LocalOperationExecutors.py
@@ -24,8 +24,6 @@ def LocalSum_np(self, subdomains):
     outarr[:,:]=arr
     # Notice we don't need to return anything, because the resulting array (arr) is copied to outsubdomain through outarr 
 
-
-
 @executor
 @localoperation
 def LocalMult_np(self, subdomains):
@@ -39,4 +37,52 @@ def LocalMult_np(self, subdomains):
     # Copy values to outarr (outsubdomain)
     outarr[:,:]=arr
 
+@executor
+@localoperation
+def LocalMaximum_np(self, subdomains):
+#finding Local Maximum among the given locations
+	outsubdomain = subdomains[0]
+	outarr = outsubdomain.get_nparray()	
+	arr= np.maximum(subdomains[1].get_nparray(),subdomains[2].get_nparray())
+	outarr[:,:]=arr
+
+@executor
+@localoperation
+#finding Local Minimum among the given locations
+def LocalMinimum_np(self, subdomains):
+	outsubdomain = subdomains[0]
+	outarr = outsubdomain.get_nparray()
+	arr = np.minimum(subdomains[1].get_nparray(),subdomains[2].get_nparray())
+	outarr[:,:]=arr
+	
+@executor
+@localoperation
+#finding Local Mean among the given locations
+def LocalMean_np(self, subdomains):
+    outsubdomain = subdomains[0]
+    outarr = outsubdomain.get_nparray()
+    arr=np.add(subdomains[1].get_nparray(),subdomains[2].get_nparray())
+    denom=len(subdomains)-1
+    outarr[:,:]=arr/denom
+
+
+@executor
+@localoperation
+#finding Local Difference among the given locations
+def LocalDifference_np(self, subdomains):
+    outsubdomain = subdomains[0]
+    outarr = outsubdomain.get_nparray()
+    arr1=np.array(subdomains[1].get_nparray())
+    arr2=np.array(subdomains[2].get_nparray())
+    arr=arr1-arr2
+    outarr[:,:]=arr
+    
+@executor
+@localoperation
+#finding Local Product among the given locations
+def LocalProduct_np(self, subdomains):
+    outsubdomain = subdomains[0]
+    outarr = outsubdomain.get_nparray()
+    arr=np.multiply(subdomains[1].get_nparray(),subdomains[2].get_nparray())
+    outarr[:,:]=arr
 
