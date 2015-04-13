07a278aa51dc89c343f72653eb5dfe3a74b88a78
Eric Shook <eshook@mcg307esL.(none)>
2015-04-13 13:47:57 -0400

    Including Microsoft Windows operating system check (PCMLConfig). Also including PCMLPrims that helps with parameterization. A few minor fixes and comments were also included.

---
+++ pcml/core/PCMLPrims.py
@@ -0,0 +1,30 @@
+"""
+Copyright (c) 2014 High-Performance Computing and GIS (HPCGIS) Laboratory. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+Authors and contributors: Eric Shook (eshook@kent.edu)
+"""
+
+# Define data structures supported in PCML
+class Datastructure():
+    """ Enumeration class. Defines the type of data structure.
+    """
+    array = 1
+    pointlist = 2
+
+# Define the type of Executor to use in the Scheduler
+class ExecutorType():
+    """ Enumeration class. It defines which executor to apply for an operation
+    """
+    serialpython = 1
+    parallelpythonqueue = 2
+
+
+class OpClass():
+    """ Enumeration class. Classifies operations as local, focal, zonal, or global.
+    """
+    localclass = 1
+    focalclass = 2
+    zonalclass = 3
+    globalclass = 4
+
+
