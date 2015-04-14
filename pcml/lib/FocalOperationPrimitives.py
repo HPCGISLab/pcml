a973ddb11b62587c273f2d078e4b151e6ec7f9e0
Gowtham Kukkadapu <gkukkada@kent.edu>
2015-04-14 07:12:20 -0400

    updated authors in FocalOperationPrimitives and LocalOPerationExecutors

--- pcml/lib/FocalOperationPrimitives.py
+++ pcml/lib/FocalOperationPrimitives.py
@@ -2,7 +2,7 @@
 Copyright (c) 2014 High-Performance Computing and GIS (HPCGIS) Laboratory. All rights reserved.
 Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 Authors and contributors: Eric Shook (eshook@kent.edu); Zhengliang Feng (odayfans@gmail.com, zfeng2@kent.edu);
-Sandeep Vutla (svutla@kent.edu, sandeepvutla@yahoo.in)
+Sandeep Vutla (svutla@kent.edu, sandeepvutla@yahoo.in); Gowtham Kukkadapu (gkukkada@kent.edu)
 """
 from ..core.Operation import *
 from ..core.Scheduler import *
--- pcml/lib/LocalOperationExecutors.py
+++ pcml/lib/LocalOperationExecutors.py
@@ -1,7 +1,7 @@
 """
 Copyright (c) 2014 High-Performance Computing and GIS (HPCGIS) Laboratory. All rights reserved.
 Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-Authors and contributors: Eric Shook (eshook@kent.edu); Zhengliang Feng (odayfans@gmail.com, zfeng2@kent.edu)
+Authors and contributors: Eric Shook (eshook@kent.edu); Zhengliang Feng (odayfans@gmail.com, zfeng2@kent.edu); Gowtham Kukkadapu(gkukkada@kent.edu)
 """
 
 from ..core.Operation import *
