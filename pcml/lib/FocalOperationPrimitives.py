86a04773b79e2d0d3f315937d0b39b8034b65339
Sandeep Vutla <svutla@kent.edu>
2015-04-13 22:29:26 -0400

    updated authorship

--- pcml/lib/FocalOperationPrimitives.py
+++ pcml/lib/FocalOperationPrimitives.py
@@ -1,7 +1,8 @@
 """
 Copyright (c) 2014 High-Performance Computing and GIS (HPCGIS) Laboratory. All rights reserved.
 Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-Authors and contributors: Eric Shook (eshook@kent.edu); Zhengliang Feng (odayfans@gmail.com, zfeng2@kent.edu)
+Authors and contributors: Eric Shook (eshook@kent.edu); Zhengliang Feng (odayfans@gmail.com, zfeng2@kent.edu);
+Sandeep Vutla (svutla@kent.edu, sandeepvutla@yahoo.in)
 """
 from ..core.Operation import *
 from ..core.Scheduler import *
